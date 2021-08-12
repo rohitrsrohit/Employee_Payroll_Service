a39d8ab8c44b5d437e7d77e9f30753fadb2184c9
Rohit Patro <rohitpatro6@gmail.com>
2021-08-12 21:12:05 +0530

    ADDED:Program To Demonstrate File Operation Added

---
+++ src/main/java/com/bridgelabz/FileUtils.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.io.File;
+
+public class FileUtils {
+    public static boolean deleteFiles(File contentsToDelete) {
+        File[] allContent = contentsToDelete.listFiles();
+        if (allContent != null) {
+            for (File file : allContent) {
+                deleteFiles(file);
+            }
+        }
+        return contentsToDelete.delete();
+    }
+}
