6f31160711e1b85b2c33de87c7c2abfb54c93772
Rohit Patro <rohitpatro6@gmail.com>
2021-08-12 21:36:29 +0530

    ADDED:Creation of Employee Payroll Service To Store Employee Payroll

---
+++ src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class EmployeePayrollServiceTest {
+    @Test
+    public void given3EmployeeWhenWrittenToFileShouldMatchEmployeeEntries() {
+        EmployeePayrollData[] arrayOfEmps = {
+                new EmployeePayrollData(1,"Jeff Bezos", 100000.0),
+                new EmployeePayrollData(2,"Bill Gates", 200000.0),
+                new EmployeePayrollData(3,"Mark Zuckerbarg", 300000.0)
+        };
+        EmployeePayrollService employeePayrollService;
+        employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+        employeePayrollService.writeEmployeePayrollData();
+        
+    }
+}
