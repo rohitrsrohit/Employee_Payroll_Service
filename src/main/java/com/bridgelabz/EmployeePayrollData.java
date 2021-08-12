b38862baa141f1f9c79e2d4edc9eddf46edd772c
Rohit Patro <rohitpatro6@gmail.com>
2021-08-12 21:02:11 +0530

    ADDED:Welcome Page Added

---
+++ src/main/java/com/bridgelabz/EmployeePayrollData.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmployeePayrollData {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Payroll Service");
+    }
+}
