cb03e922e7acf720f54d90899f1af7f7418cac3b
Rohit Patro <rohitpatro6@gmail.com>
2021-08-12 21:06:52 +0530

    ADDED:Creation of Employee Payroll Added

--- src/main/java/com/bridgelabz/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/EmployeePayrollData.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
 
 public class EmployeePayrollData {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Employee Payroll Service");
+    public int id;
+    public String name;
+    public double salary;
+
+    public EmployeePayrollData(Integer id, String name, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary;
     }
 }
+
